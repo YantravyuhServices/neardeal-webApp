06b12225923582d7af8abc9faf5dc480653990d7
Bharat Sharma <bharatsharma98971@gmail.com>
2024-10-19 23:53:05 +0530

    Add pagination navigation to Booking component

--- client/src/Pages/Booking.jsx
+++ client/src/Pages/Booking.jsx
@@ -435,6 +435,28 @@ const Booking = () => {
                   )}
                 </tbody>
               </table>
+
+              <nav>
+                <ul className="pagination justify-content-center">
+                  <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
+                    <a className="page-link" href="#" onClick={() => handlePageChange(currentPage - 1)}>
+                      Previous
+                    </a>
+                  </li>
+                  {[...Array(totalPages)].map((_, index) => (
+                    <li className={`page-item ${currentPage === index + 1 ? 'pagination' : ''}`} key={index}>
+                      <a className="page-link" href="#" onClick={() => handlePageChange(index + 1)}>
+                        {index + 1}
+                      </a>
+                    </li>
+                  ))}
+                  <li className={`page-item ${currentPage === totalPages ? 'disabled' : ''}`}>
+                    <a className="page-link" href="#" onClick={() => handlePageChange(currentPage + 1)}>
+                      Next
+                    </a>
+                  </li>
+                </ul>
+              </nav>
             </motion.div>
           )}
 
